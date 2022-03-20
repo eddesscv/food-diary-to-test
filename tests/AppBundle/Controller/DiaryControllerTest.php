fdc401265c1e5ab334e21ed01cd9a7555390da53
eddesscv <ednilsonmendes@outlook.com>
2022-03-20 12:24:54 +0100

    testAddRecord

--- tests/AppBundle/Controller/DiaryControllerTest.php
+++ tests/AppBundle/Controller/DiaryControllerTest.php
@@ -39,4 +39,28 @@ class DiaryControllerTest extends WebTestCase
 
         $this->assertSame(200, $client->getResponse()->getStatusCode()); // N'oublions pas que nous sommes dans le cadre d'un test ! Il nous faut donc ajouter une assertion. Il faut savoir que la classe Symfony\Bundle\FrameworkBundle\Test\WebTestCase étend la classe PHPUnit PHPUnit_Framework_Test nous permettant d'utiliser les méthodes pour écrire des assertions. 
     }
+
+    public function testHomepage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(1, $crawler->filter('html:contains("Bienvenue sur FoodDiary !")')->count());
+        /* $this->assertSame(1, $crawler->filter('h1')->count()); */
+    }
+
+    public function testAddRecord()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/diary/add-new-record');
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['food[username]'] = 'John Doe';
+        $form['food[entitled]'] = 'Plat de pâtes';
+        $form['food[calories]'] = 600;
+        $crawler = $client->submit($form);
+
+        echo $client->getResponse()->getContent();
+    }
 }
