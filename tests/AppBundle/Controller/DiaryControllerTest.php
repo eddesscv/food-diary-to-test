3ad46bd10b5d8488044cc01b4366cd3f3bcfc514
eddesscv <ednilsonmendes@outlook.com>
2022-03-20 12:48:34 +0100

    ok

--- tests/AppBundle/Controller/DiaryControllerTest.php
+++ tests/AppBundle/Controller/DiaryControllerTest.php
@@ -53,7 +53,12 @@ class DiaryControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/diary/add-new-record');
+        /* $crawler = $client->request('GET', '/diary/add-new-record'); */
+
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Voir tous les rapports')->link();
+        $crawler = $client->click($link);
 
         $form = $crawler->selectButton('Ajouter')->form();
         $form['food[username]'] = 'John Doe';
@@ -61,6 +66,20 @@ class DiaryControllerTest extends WebTestCase
         $form['food[calories]'] = 600;
         $crawler = $client->submit($form);
 
-        echo $client->getResponse()->getContent();
+        /* $client->followRedirect();
+
+        echo $client->getResponse()->getContent(); */
+
+        /* $crawler = $client->followRedirect(); */ // Attention à bien récupérer le crawler mis à jour
+
+        /* $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+ */
+        $link = $crawler->selectLink('Voir tous les rapports')->link();
+        $crawler = $client->click($link);
+
+        $info = $crawler->filter('h1')->text();
+        $info = $string = trim(preg_replace('/\s\s+/', ' ', $info)); // On retire les retours à la ligne pour faciliter la vérification
+
+        $this->assertSame("Tous les rapports Tout ce qui a été mangé !", $info);
     }
 }
